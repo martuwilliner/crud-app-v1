9c531441a909f53c9a0139f77469ed77989cad3a
martuwilliner <williner.martin@gmail.com>
2021-10-20 01:19:04 -0300

    CRUD Insersión de datos

--- src/components/CrudApp.js
+++ src/components/CrudApp.js
@@ -59,11 +59,31 @@ const initialDb= [
 
 const CrudApp = () => {
     const [db, setDb] = useState(initialDb);
+
+    const [dataToEdit, setDataToEdit] = useState(null)
+
+    const createData = (data) => {
+        data.id = Date.now();
+        setDb([...db,data])
+    }
+
+    const updateData = (data) => {}
+
+    const deleteData = (id) => {} 
+
     return (
         <div>
             <h2>CRUD App</h2>
-            <CrudForm/>
-            <CrudTable data={db}/>
+            <CrudForm
+            createData={createData}
+            updateData={updateData}
+            dataToEdit={dataToEdit}
+            setDataToEdit={setDataToEdit}
+            />
+            <CrudTable data={db}
+            deleteData={deleteData}
+            setDataToEdit={setDataToEdit}
+            />
         </div>
     )
 }
--- src/components/CrudForm.js
+++ src/components/CrudForm.js
@@ -6,16 +6,38 @@ const initialForm = {
     id: null
 }
 
-const CrudForm = () => {
+const CrudForm = ({createData,updateData, dataToEdit, setDataToEdit}) => {
     const [form, setForm] = useState({initialForm});
 
     const handleChange = (e) => {
-
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        })
     }
 
-    const handleSubmit = (e) => {}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
 
-    const handleReset = (e) => {}
+        if(!form.name || !form.lastname){ /* si esos campos no tienen valores */
+            alert("Datos incompletos")
+            return;
+        }
+
+        if(form.id === null){
+            createData(form)
+        } else {
+            updateData(form)
+        }
+
+        handleReset();
+    }
+
+    const handleReset = (e) => {
+        setForm(initialForm);
+        setDataToEdit(null);
+    }
     
     return (
         <div>
